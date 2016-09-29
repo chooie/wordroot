b8db711b42303d0edb60794b28ab30816d03bcfc
Charlie Hebert <charlie.hebert92@gmail.com>
2016-09-28 23:42:47 -0600

    Update styles for nav and page. Add functionality to highlight active nav link

--- src/frontend/wordroot/components/navbar/core.cljs
+++ src/frontend/wordroot/components/navbar/core.cljs
@@ -1,17 +1,26 @@
 (ns wordroot.components.navbar.core
   (:require
+   (reagent.session :as session)
    (wordroot.constants :as constants)))
 
 (defn link
   [href label]
   [:li.site-link
    [:a
-    {:href (str "/" constants/secretary-prefix href)}
+    {:href  (str "/" constants/secretary-prefix href)
+     :class (let [page-keyword (session/get :page)
+                  page-string  (name page-keyword)
+                  to-lower     clojure.string/lower-case
+                  page         (to-lower page-string)
+                  label        (to-lower label)]
+              (when (= page label)
+                "active"))}
     label]])
 
 (defn navbar-component
   []
-  [:nav
-   [:ul
-    [link (:home constants/paths) "Home"]
-    [link (:about constants/paths) "About"]]])
+  [:nav.navbar
+   [:div.row
+    [:ul.site-links
+     [link (:home constants/paths) "Home"]
+     [link (:about constants/paths) "About"]]]])
