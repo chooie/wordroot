48b05c22706645af24232afd85e950d291fca8f2
Charlie Hebert <charlie.hebert92@gmail.com>
2016-09-28 23:51:03 -0600

    Separate concerns in link component

--- src/frontend/wordroot/components/navbar/core.cljs
+++ src/frontend/wordroot/components/navbar/core.cljs
@@ -3,18 +3,22 @@
    (reagent.session :as session)
    (wordroot.constants :as constants)))
 
+(defn make-active-when-active
+  [label]
+  (let [page-keyword (session/get :page)
+        page-string  (name page-keyword)
+        to-lower     clojure.string/lower-case
+        page         (to-lower page-string)
+        label        (to-lower label)]
+    (when (= page label)
+      "active")))
+
 (defn link
   [href label]
   [:li.site-link
    [:a
     {:href  (str "/" constants/secretary-prefix href)
-     :class (let [page-keyword (session/get :page)
-                  page-string  (name page-keyword)
-                  to-lower     clojure.string/lower-case
-                  page         (to-lower page-string)
-                  label        (to-lower label)]
-              (when (= page label)
-                "active"))}
+     :class (make-active-when-active label)}
     label]])
 
 (defn navbar-component
