a70090cacbe74993cae3de7a5b5efac2151f8f1c
Charlie Hebert <charlie.hebert92@gmail.com>
2016-10-02 23:40:57 -0600

    Add highlighting for active word in words navbar and some styling

--- src/frontend/wordroot/components/pages/home/words_navbar.cljs
+++ src/frontend/wordroot/components/pages/home/words_navbar.cljs
@@ -2,12 +2,15 @@
 
 (defn component
   [words current-word-index-atom]
-  [:aside
-   [:ul.words
-    (map-indexed
-      (fn [index word]
-        ^{:key index}
-        [:li.word
-         {:on-click #(reset! current-word-index-atom index)}
-         (apply str (:parts word))])
-      words)]])
+  (let [current-index @current-word-index-atom]
+    [:aside.words-side-bar
+     [:ul.words
+      (map-indexed
+        (fn [index word]
+          ^{:key index}
+          [:li.word
+           {:on-click #(reset! current-word-index-atom index)
+            :class    (doall (when (= current-index index)
+                               "active"))}
+           (apply str (:parts word))])
+        words)]]))
