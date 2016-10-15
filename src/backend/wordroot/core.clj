336204200c4ca2fc5fa8e847be475ef6775af3a9
Charlie Hebert <charlie.hebert92@gmail.com>
2016-10-14 19:41:25 -0600

    Serve up index file at root URI

--- src/backend/wordroot/core.clj
+++ src/backend/wordroot/core.clj
@@ -5,10 +5,17 @@
    [compojure.handler :as handler]
    [ring.middleware.resource :as resource]
    [ring.middleware.content-type :as content-type]
-   [ring.middleware.not-modified :as not-modified]))
+   [ring.middleware.not-modified :as not-modified]
+   [ring.util.response :as response]))
 
 (compojure/defroutes main-routes
-  (compojure/GET "/" [] "Hello")
+  (compojure/GET "/" []
+    (let [index-file-response (response/resource-response
+                                "index.html"
+                                {:root "public"})]
+      (response/content-type
+        index-file-response
+        "text/html")))
   (route/not-found "Page not found"))
 
 (def app
