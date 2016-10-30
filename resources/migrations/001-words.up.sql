c37cd60266223c9061100f1c0ceade698b1cf3ff
Charlie Hebert <charlie.hebert92@gmail.com>
2016-10-29 22:45:18 -0600

    Add more tables for words

--- resources/migrations/001-words.up.sql
+++ resources/migrations/001-words.up.sql
@@ -3,3 +3,27 @@ id SERIAL PRIMARY KEY,
 word VARCHAR(64) NOT NULL UNIQUE,
 meaning VARCHAR(512) NOT NULL
 );
+
+CREATE TABLE languages (
+id SERIAL PRIMARY KEY,
+name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE roots (
+id SERIAL PRIMARY KEY,
+word VARCHAR(64) NOT NULL,
+meaning VARCHAR(512) NOT NULL,
+language_id INTEGER REFERENCES languages(id)
+);
+
+CREATE TABLE word_parts (
+id SERIAL PRIMARY KEY,
+part VARCHAR(32) NOT NULL,
+word_id INTEGER REFERENCES words(id),
+position_index smallint NOT NULL
+);
+
+CREATE TABLE word_parts_roots (
+word_part_id INTEGER REFERENCES word_parts(id),
+root_id INTEGER REFERENCES roots(id)
+);
