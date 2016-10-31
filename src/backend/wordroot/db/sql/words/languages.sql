d4760d635d6e6969110b661a670b6cc519340a01
Charlie Hebert <charlie.hebert92@gmail.com>
2016-10-30 18:21:47 -0600

    Add additional language SQL statements

--- src/backend/wordroot/db/sql/words/languages.sql
+++ src/backend/wordroot/db/sql/words/languages.sql
@@ -2,3 +2,13 @@
 -- :doc Insert a language
 INSERT INTO languages (name)
 VALUES (:name);
+
+-- :name get-language-by-id :? :1
+-- :doc Get language name by id
+SELECT * FROM languages
+WHERE id = :id
+
+-- :name get-language-id-by-name :? :1
+-- :doc Get language id with the given name
+SELECT * FROM languages
+WHERE name = :name
