4f7b77525d764a3edf3f705f4b16e131526cb1f2
Charlie Hebert <charlie.hebert92@gmail.com>
2016-11-25 20:11:12 -0700

    Apply classes to word parts in word header based on the root index rather than word part index

--- src/frontend/wordroot/components/pages/home/word_view.cljs
+++ src/frontend/wordroot/components/pages/home/word_view.cljs
@@ -3,21 +3,18 @@
 (def colour-classes ["colour-1" "colour-2" "colour-3"])
 (def line-classes ["underline" "overline"])
 
-(defn isNan?
-  [n]
-  (js/isNaN n))
-
 (defn get-class-at-index
   [classes index]
-  (let [modded-i (mod index (count classes))
-        class    (get classes modded-i)]
-    class))
+  (when index
+    (let [modded-i (mod index (count classes))
+          class    (get classes modded-i)]
+      class)))
 
 (defn get-word-part-classes
-  [colour-classes list-classes index]
-  (let [colour-class (get-class-at-index colour-classes index)
-        line-class   (get-class-at-index line-classes index)]
-    (clojure.string/join " " [colour-class line-class])))
+  [colour-classes list-classes index-in-word root-index]
+  (let [line-class   (get-class-at-index line-classes root-index)
+        colour-class (get-class-at-index colour-classes root-index)]
+    (clojure.string/join " " [line-class colour-class])))
 
 (defn prepare-part
   [part index]
@@ -26,22 +23,34 @@
     part))
 
 (defn word-part-component
-  [colour-classes line-classes part index]
+  [colour-classes line-classes part index-in-word
+   root-index]
   ^{:key part}
   [:li.word-part
-   {:class (get-word-part-classes colour-classes line-classes index)}
-   (prepare-part part index)])
+   {:class (get-word-part-classes colour-classes line-classes index-in-word
+             root-index)}
+   (prepare-part part index-in-word)])
 
 (defn word-parts-header
   [word-parts]
   {:pre [(> (count word-parts) 0)]}
-  [:div.word-header.center
-   [:ul.word-parts
-    (map-indexed
-      (fn [i word-part]
-        (let [{:keys [part root]} word-part]
-          (word-part-component colour-classes line-classes part i)))
-      word-parts)]])
+  (let [next-index               (atom 0)
+        words-with-roots-indexes (map
+                                   (fn [word-part]
+                                     (if (:root word-part)
+                                       (do
+                                         (swap! next-index inc)
+                                         [word-part (dec @next-index)])
+                                       [word-part nil]))
+                                   word-parts)]
+    [:div.word-header.center
+     [:ul.word-parts
+      (map-indexed
+        (fn [index-in-word [word-part root-index]]
+          (let [{:keys [part]} word-part]
+            (word-part-component colour-classes line-classes part index-in-word
+              root-index)))
+        words-with-roots-indexes)]]))
 
 (defn root-component
   [root]
