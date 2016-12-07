fc646e0afbc6eba919574c7be0d3f93e01e55336
Charlie Hebert <charlie.hebert92@gmail.com>
2016-12-06 21:27:33 -0700

    Reintroduce run-frontend-tests boot task. However, must run this as part of the other tasks that build the fileset

--- boot_tasks/wordroot_tasks/test.clj
+++ boot_tasks/wordroot_tasks/test.clj
@@ -1,5 +1,7 @@
 (ns wordroot-tasks.test
   (:require
+   [boot.core :as boot]
+   [crisptrutski.boot-cljs-test :as boot-cljs-test]
    [clojure.test :as test]))
 
 (defn run-backend-tests
@@ -7,8 +9,9 @@
   (test/run-all-tests #"^wordroot.*-test$"))
 
 ;; TODO: Figure out how to use this without it being unbearably slow
-#_(boot/deftask run-frontend-tests
-    []
-    (comp
-      (boot-cljs-test/test-cljs
-        :js-env :phantom)))
+;; Figure out how to use this without wiping out the fileset
+(boot/deftask run-frontend-tests
+  []
+  (comp
+    (boot-cljs-test/test-cljs
+      :js-env :phantom)))
