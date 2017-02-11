5c53ef74d3fed87b4b3efb33e786f2f1d41d1cd5
Charlie Hebert <charlie.hebert92@gmail.com>
2017-02-10 21:37:55 -0700

    Extract test-element creation

--- src/frontend/wordroot/components/core_test.cljs
+++ src/frontend/wordroot/components/core_test.cljs
@@ -4,13 +4,18 @@
    [reagent.core :as r]
    [wordroot.components.core :as wordroot-core]))
 
+(defn create-test-element
+  []
+  (let [document-body (.-body js/document)
+        test-element  (.createElement js/document "div")]
+    (.appendChild document-body test-element)
+    test-element))
+
 (deftest root-app
   (testing "Reagent element renders correctly"
-    (let [document-body (.-body js/document)
-          test-element  (.createElement js/document "div")
-          _             (.appendChild document-body test-element)
-          mounted-app   (r/render
-                          [:div#app "Hey"]
-                          test-element)
-          application   (.getElementById js/document "app")]
+    (let [test-element (create-test-element)
+          mounted-app  (r/render
+                         [:div#app "Hey"]
+                         test-element)
+          application  (.getElementById js/document "app")]
       (is (= "Hey" (.-innerHTML application))))))
