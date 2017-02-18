2f21b8bfa17d21af10ae6ffe22e51044508b98da
Charlie Hebert <charlie.hebert92@gmail.com>
2017-02-17 19:46:12 -0700

    Fix word-view root-to-show-index-atom resetting on code reload

--- src/frontend/wordroot/ui/pages/home/word_view/core.cljs
+++ src/frontend/wordroot/ui/pages/home/word_view/core.cljs
@@ -4,12 +4,13 @@
    [wordroot.ui.pages.home.word-view.header :as header]
    [wordroot.ui.pages.home.word-view.roots-list :as roots-list]))
 
+(defonce root-to-show-index-atom (reagent/atom nil))
+
 (defn component
   [word]
-  (let [root-to-show-index-atom (reagent/atom nil)]
-    [:div
-     [header/component (:parts word) root-to-show-index-atom]
-     [:div.roots.center
-      [roots-list/component (:parts word) root-to-show-index-atom]]
-     [:div.center
-      [:p (:meaning word)]]]))
+  [:div
+   [header/component (:parts word) root-to-show-index-atom]
+   [:div.roots.center
+    [roots-list/component (:parts word) root-to-show-index-atom]]
+   [:div.center
+    [:p (:meaning word)]]])
