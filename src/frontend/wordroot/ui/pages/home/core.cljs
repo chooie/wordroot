670473b4ab84ff683b6bde32e498b1765e961ddc
Charlie Hebert <charlie.hebert92@gmail.com>
2017-03-10 17:25:34 -0700

    Fix issue where word messages weren't displaying correctly depending on whether words could be loaded or not

--- src/frontend/wordroot/ui/pages/home/core.cljs
+++ src/frontend/wordroot/ui/pages/home/core.cljs
@@ -30,6 +30,14 @@
 (defonce menu-is-open?-atom (reagent/atom false))
 (defonce get-words-has-been-run? (atom false))
 
+#_(add-watch words-atom :watcher
+    (fn [key atom old-state new-state]
+      (.log js/console "-- Atom Changed --")
+      (.log js/console "key" key)
+      (.log js/console "atom" atom)
+      (.log js/console "old-state" old-state)
+      (.log js/console "new-state" new-state)))
+
 (defn get-words!
   [base-url]
   (let [words-url (str base-url "/words")]
@@ -37,27 +45,42 @@
       {:handler       (fn [words]
                         (reset! words-atom words))
        :error-handler (fn [error]
+                        (reset! words-atom :error-getting-words)
                         (println "Error getting words:" error))})))
+
+(defn loading-component
+  []
+  [:div
+   [:h1 "Loading..."]])
+
+(defn words-error-component
+  []
+  [:div
+   [:h1 "Error! We couldn't get any words :("]])
+
+(defn render-words
+  [words active-word]
+  (if (= words :error-getting-words)
+    words-error-component
+    (if (= (count words) 0)
+      [:h1 "There aren't any words to show..."]
+      [:div
+       [words-navbar/component words
+        current-word-index-atom
+        menu-is-open?-atom]
+       [:div.controls
+        [words-menu-toggle menu-is-open?-atom]]
+       [:h1 "Home"]
+       [:div.clear]
+       [word-view/component active-word]])))
 
 (defn home-page
   [base-url]
   (when (not @get-words-has-been-run?)
     (get-words! base-url)
     (reset! get-words-has-been-run? true))
-  (let [words @words-atom
-        word  (get words @current-word-index-atom)]
-    (if ((complement nil?) words)
-      (if (> (count words) 0)
-        [:div
-         [words-navbar/component words
-          current-word-index-atom
-          menu-is-open?-atom]
-         [:div.controls
-          [words-menu-toggle menu-is-open?-atom]]
-         [:h1 "Home"]
-         [:div.clear]
-         [word-view/component word]]
-        [:div
-         [:h1 "Error! We couldn't find any words :("]])
-      [:div
-       [:h1 "Loading..."]])))
+  (let [words       @words-atom
+        active-word (get words @current-word-index-atom)]
+    (if (nil? words)
+      [loading-component]
+      [render-words words active-word])))
