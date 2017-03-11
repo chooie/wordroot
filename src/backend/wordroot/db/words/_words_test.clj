ebc13caf80786277d213803cfd6a2afe95e63fca
Charlie Hebert <charlie.hebert92@gmail.com>
2017-03-11 12:47:49 -0700

    Testing getting all words

--- src/backend/wordroot/db/words/_words_test.clj
+++ src/backend/wordroot/db/words/_words_test.clj
@@ -8,35 +8,19 @@
    [wordroot.db.words.example-words :as wordroot-db-example-words]
    [wordroot.db.words.words :as words]))
 
-(def test-word
-  {:word    "thisisareallylongword"
-   :meaning "Some meaning about a really long word"
-   :parts   [{:part "this"
-              :root {:word     "root1"
-                     :meaning  "meaning1"
-                     :language "language1"}}
-             {:part "is"}
-             {:part "a"}
-             {:part "really"
-              :root {:word     "root2"
-                     :meaning  "meaning2"
-                     :language "language2"}}
-             {:part "long"}
-             {:part "word"
-              :root {:word     "root3"
-                     :meaning  "meaning3"
-                     :language "language3"}}]})
-
 (deftest db-test
   (let [config               (config/get-config-with-profile :automated-tests)
         db-config            (:db config)
         db-component         (wordroot-db/new-db {:connection db-config})
         started-db-component (component/start db-component)
-        db-connection        (:connection started-db-component)]
+        db-connection        (:connection started-db-component)
+        test-word            (get wordroot-db-example-words/words 0)]
     (migration-management/rollback! db-connection)
     (migration-management/migrate! db-connection)
     (testing "Can insert a word into the database and get it back again"
       (words/persist-word! db-connection test-word)
       (let [word-from-db (words/get-word-by-word-name
                            db-connection (:word test-word))]
-        (is (= (:word test-word) (:word word-from-db)))))))
+        (is (= (:word test-word) (:word word-from-db)))
+        (let [words (words/get-words db-connection)]
+          (is (= (count words) 1)))))))
